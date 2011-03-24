78e52a21e859eae4d1950d7695d9ac980c88364d
Jonathan Quigg <jonathan.quigg@gd-ais.com>
2011-03-24 09:32:07 -0400

    updated behavior to allow resource.action another_resource instead of resource.action association_table; and comments

--- lib/relatable.rb
+++ lib/relatable.rb
@@ -13,32 +13,78 @@ module Relatable
                         belongs_to \"#{targetable}\", :polymorphic => true" )
     end
   end
-  
-  def instance_acts_as(associations,relationships)
-      relationship          = relationships[:as].to_sym
+ 
+  # TODO: The construction of the Association within the instance creates an issue with self-referential relationships:
+  #  For example:
+  #   e = Email.new
+  #   e.behaves_like :drops, ... etc.
+  #   ...Now how does e refer to itself if it does something to itself?  A couple of ways to solve this:
+  #      1. Create a self-reflection method.
+  #      2. Provide the original behavior in the master branch s.t. e.drops a, where 'a' is an 'Association'
+  #
+  #   Perhaps the way to handle this is:
+  #      if related_model is self, then alter related_model s.t. a self-reference can be made:  e.g., e.drops e, the "e" being dropped is self, and can be checked for.
+  #      related_model can be altered
+  #      whether or not e can drop e is verified by self.can_drop?(e) 
+ 
+  def behaves_like(action,relationships)
+      @action, @inverse_action, @relationship, @association_table = action.to_sym, relationships[:inverse].to_sym, relationships[:as].to_sym, relationships[:via]
 
+
+      #Enter eigenclass to define singletons
       eigenclass.class_eval do
 
-          define_method(("#{associations}").to_sym) do |a|
-            a_namified = a.class.to_s.underscore.pluralize
+          #Define a 'can_<action>?' singleton:
+          #  
+          # iff @action => :drops, for e, then e.can_drop? will be defined.
+          #   e.can_drop?(p) in this case, e and p actions must be relatable, the relationship and inverse relationship must match, and they must share an association table.
 
-            self.save 
+          define_method( ("can_#{action.to_s.singularize}?").to_sym ) do |related_model|
+            begin
+              ( related_model.instance_variable_get(:@inverse_action)     == @action ) &&             
+              ( related_model.instance_variable_get(:@relationship)       == inverse_as(@relationship) ) &&          
+              ( related_model.instance_variable_get(:@association_table)  == @association_table ) #self.instance_variable_get(:@association_table) )            
+            rescue
+              raise "Action relationship is undefined."
+            end
+          end
+
+          define_method(("#{action}").to_sym) do |related_model|
+
+            #guards
+            #can source actually operate on related_model?
+
+
+            #Behavior here s.t. a new association is created for each action:
+            #
+            #  e.behaves_like :drops ...
+            #  p.behaves_like :receives ...
+            #
+            #  e.drops p <--- new association instance is created.
+            #  e.drops p <--- another new association instance.
+
+            association_name      = @association_table.to_s.underscore.pluralize
+            association_instance  = @association_table.new
+
+            #self.save #perhaps unnecessary
 
             eigenclass.class_eval do 
 
-              define_method(a_namified) do
+              define_method(action.to_s) do
                 begin
-                  e.send(:remove_method,a_namified)
+                  e.send(:remove_method,association_name) #the method may already be defined.  accounts for change in relationship (e.g., :sourcable to :targetable
                 rescue
-                  a.class.send(:where,(relationship.to_s + "_id").to_sym => self.id, (relationship.to_s + "_type").to_sym => self.class ) 
+                  a.class.send(:where,(@relationship.to_s + "_id").to_sym => self.id, (@relationship.to_s + "_type").to_sym => self.class ) 
                 end
               end
             end
 
-            a.send("#{ (relationship.to_s + "_id=").to_sym }",self.id)
-            a.send("#{ (relationship.to_s + "_type=").to_sym}",self.class.to_s)
-            a.send("#{ (relationship.to_s + "_action=").to_sym}",associations.to_s)
-            a.save 
+            #Does a respond?
+            association_instance.send("#{ (@relationship.to_s + "_id=").to_sym }",self.id)
+            association_instance.send("#{ (@relationship.to_s + "_type=").to_sym}",self.class.to_s)
+            association_instance.send("#{ (@relationship.to_s + "_action=").to_sym}",action.to_s)
+
+            association_instance.save #perhaps unnecessary
 
           end
       end
@@ -53,6 +99,15 @@ module Relatable
 
   def self.included(base)
     base.extend(ClassMethods)
+  end
+
+  def is_association_table?(constant)
+
+  end
+
+
+  def inverse_as(action)
+    action == :sourcable ? :targetable : :sourcable
   end
 
 end
